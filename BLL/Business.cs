21603095cb860e3341dde49d3ce9834165fae958
Rémi Jonathan Choquette <contact@remijonathan.com>
2019-09-17 08:44:14 -0400

    Completed BLL

---
+++ BLL/Business.cs
@@ -0,0 +1,18 @@
+﻿using Entities;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class Business
+    {
+        public List<Memo> GetMemos()
+        {
+            return DB.memos;
+        }
+    }
+}
